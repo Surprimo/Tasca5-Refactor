20092024a30d5cc6b685d0ba18b7d0f530db615a
Surpirmo <alepasdie@alu.edu.gva.es>
2023-05-22 12:14:12 +0200

    Test Fly Spider

--- src/TDDSongTest.java
+++ src/TDDSongTest.java
@@ -1,12 +1,28 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class TDDSongTest {
+    //Test para fly
   @org.junit.jupiter.api.Test
     void testFly() {
         TDDSong song = new TDDSong();
         String[] cadena = {"fly"};
         String expected = "There was an old lady who swallowed a fly.\n"+
                 "I don't know why she swallowed a fly - perhaps she'll die!\n";
+        String actual = song.TDDSong(cadena);
+        assertEquals(expected, actual);
+    }
+    //Test para fly y spider
+    @org.junit.jupiter.api.Test
+    void testFlySpider() {
+        TDDSong song = new TDDSong();
+        String[] cadena = {"fly", "spider"};
+        String expected = "There was an old lady who swallowed a fly.\n"+
+                "I don't know why she swallowed a fly - perhaps she'll die!\n"+
+                "\n" +
+                "There was an old lady who swallowed a spider;\n" +
+                "That wriggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly- perhaps she'll die!\n";
         String actual = song.TDDSong(cadena);
         assertEquals(expected, actual);
     }
